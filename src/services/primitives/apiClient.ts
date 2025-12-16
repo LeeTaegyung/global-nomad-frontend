08138eef538e964ea7be67ddd14f232054353ddc
tasi <taegyung.e@gmail.com>
2025-12-16 14:43:53 +0900

    chore: 환경변수명 수정

--- src/services/primitives/apiClient.ts
+++ src/services/primitives/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const baseURL = isProduction
-  ? process.env.NEXT_PUBLIC_API_URL
+  ? process.env.NEXT_PUBLIC_ORIGIN_URL
   : 'http://localhost:3000';
 
 export const apiClient = axios.create({
