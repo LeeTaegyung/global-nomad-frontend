e698ba4011aa6064cfbd8da4f3550c838feb457e
tasi <taegyung.e@gmail.com>
2026-01-05 21:12:25 +0900

    refactor: 리다이렉트 로직의 로그인 여부 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,27 +10,31 @@ const PUBLIC_PATH_PATTERNS = [/^\/$/, /^\/detail\/\d+$/];
 
 export function middleware(req: NextRequest) {
   const { pathname, origin } = req.nextUrl;
-  const accessToken = req.cookies.get('accessToken')?.value;
-
-  const isAuthPath = AUTH_PATHS.includes(pathname);
-  const isPublicPath = PUBLIC_PATH_PATTERNS.some((regex) =>
-    regex.test(pathname)
-  );
 
   // 라우트 핸들러 요청은 리다이렉트 처리에서 제외
   if (pathname.startsWith('/api')) {
     return NextResponse.next();
   }
 
+  const isAuthPath = AUTH_PATHS.includes(pathname);
+  const isPublicPath = PUBLIC_PATH_PATTERNS.some((regex) =>
+    regex.test(pathname)
+  );
+
+  const accessToken = req.cookies.get('accessToken')?.value;
+  const refreshToken = req.cookies.get('refreshToken')?.value;
+  const isUnauthenticated =
+    accessToken === undefined && refreshToken === undefined;
+
   // 로그인시
-  if (accessToken) {
+  if (!isUnauthenticated) {
     // auth 페이지(로그인/회원가입) 라우트시
     if (isAuthPath) {
       return NextResponse.redirect(`${origin}/`);
     }
   }
   // 로그아웃시
-  if (!accessToken) {
+  if (isUnauthenticated) {
     // 프로텍트 페이지 (/, /detail, /login, /signup 제외) 접속시
     if (!isPublicPath && !isAuthPath) {
       // 로그인페이지로 리다이렉트, 이때 params에 ?&redirect_uri=현재페이지경로를 추가해줘서 로그인 완료시 접속시도했던 페이지로 리다이렉트되도록 처리
